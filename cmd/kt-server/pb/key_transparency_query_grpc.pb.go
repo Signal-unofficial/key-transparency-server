f356d9581c4184b07eef90d8735a600714252640
Vessel9817 <151808241+Vessel9817@users.noreply.github.com>
2025-11-12 05:22:51 -0500

    Fixed typo
    Signed-off-by: Vessel9817 <151808241+Vessel9817@users.noreply.github.com>

--- cmd/kt-server/pb/key_transparency_query_grpc.pb.go
+++ cmd/kt-server/pb/key_transparency_query_grpc.pb.go
@@ -142,7 +142,7 @@ type UnsafeKeyTransparencyQueryServiceServer interface {
 }
 
 func RegisterKeyTransparencyQueryServiceServer(s grpc.ServiceRegistrar, srv KeyTransparencyQueryServiceServer) {
-	// If the following call pancis, it indicates UnimplementedKeyTransparencyQueryServiceServer was
+	// If the following call panics, it indicates UnimplementedKeyTransparencyQueryServiceServer was
 	// embedded by pointer and is nil.  This will cause panics if an
 	// unimplemented method is ever invoked, so we test this at initialization
 	// time to prevent it from happening at runtime later due to I/O.
